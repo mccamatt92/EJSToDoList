3ed12a04d5a8aa01e55b55005919796607a2fd22
mccamatt92 <matthias.mccarthy@gmail.com>
2020-10-26 21:49:13 +0100

    Added Layouts

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var workItems = [];
 app.get("/",function(req,res){
 
     var today = new Date();
-    var currentDate = today.getDay();
     var options ={
         weekday:"long",
         day:"numeric",
@@ -24,21 +23,31 @@ app.get("/",function(req,res){
     res.render("list", { listTitle: day, newListItems: items })
 })
 
+app.get("/about", function (req, res) {
+res.render("about");
+});
+
 app.get("/work", function (req, res) {
-res.render("list",{listTitle:"Work List",newListItems:workItems})
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.post("/",function(req,res){
-  var item =  req.body.newItem;
-  items.push(item);
+    console.log(req.body)
+    let item = req.body.newItem;
+    if(req.body.list === "Work")
+{
+        workItems.push(item);
+        res.redirect("/work")
+}
+else
+{
+      items.push(item);
    res.redirect("/");
+}
+
+
 })
 
-app.post("/work",function(req,res){
-    let item = req.body.item;
-    workItems.push(item);
-    res.redirect("/work")
-})
 
 app.listen(3000,function(){
     console.log("Server started on port 3000")
